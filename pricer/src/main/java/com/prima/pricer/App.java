d408a969721826d7c2532b0e52525973c9795a04
Vasyl <leo124@bigmir.net>
2016-09-17 12:05:11 +0300

    add tasks

--- pricer/src/main/java/com/prima/pricer/App.java
+++ pricer/src/main/java/com/prima/pricer/App.java
@@ -1,15 +1,9 @@
 package com.prima.pricer;
 
-import org.apache.log4j.BasicConfigurator;
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
-
 public class App {
-
-	protected final static Logger logger = LogManager.getLogger(App.class);
-	
-	public static void main(String[] args) {
-		BasicConfigurator.configure();
-		logger.info("1");
-	}
+    //TODO: Read properties config.
+    //TODO: Read XML config for each priceBook.
+    //TODO: Convert xls to xlsx files.
+    //TODO: Loop #1:
+    //
 }
