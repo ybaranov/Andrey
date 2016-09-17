757caa21b5a2177fe7dec5b4ac4a873728b6dde2
ybaranov <baranovy@ukr.net>
2016-09-17 13:02:28 +0300

    added ignore

--- pricer/src/main/java/com/prima/pricer/service/PriceBookReaderService.java
+++ pricer/src/main/java/com/prima/pricer/service/PriceBookReaderService.java
@@ -2,7 +2,6 @@ package com.prima.pricer.service;
 
 import com.prima.pricer.interfaces.PriceBookReaderFacade;
 
-//вычитка из файла
 public class PriceBookReaderService implements PriceBookReaderFacade {
 
 }
--- pricer/src/main/java/com/prima/pricer/service/PriceBookWriterService.java
+++ pricer/src/main/java/com/prima/pricer/service/PriceBookWriterService.java
@@ -2,6 +2,5 @@ package com.prima.pricer.service;
 
 import com.prima.pricer.interfaces.PriceBookWriterFacade;
 
-//вычитка из файла
 public class PriceBookWriterService implements PriceBookWriterFacade {
 }
